fbfcfdfe79f2fe2fcb55a59c3d49f486eae4256c
Renat Skosarev <skosarrrev@gmail.com>
2023-10-27 22:40:09 +0500

    Make students.csv autoload from /resources

--- src/main/java/com/skosarev/task01/Main.java
+++ src/main/java/com/skosarev/task01/Main.java
@@ -6,10 +6,8 @@ import com.skosarev.task01.service.StudentService;
 
 public class Main {
     public static void main(String[] args) {
-        // лучше задавать путь в аргументах запуска, но для удобства (чтобы не вводить каждый раз) вынес сюда
-        CommandBuilder commandBuilder = new CommandBuilder(new StudentService(new CSVDataLoader(
-                ""
-        )));
+        String path = ClassLoader.getSystemClassLoader().getResource("students.csv").getPath();
+        CommandBuilder commandBuilder = new CommandBuilder(new StudentService(new CSVDataLoader(path)));
 
         commandBuilder.solve(args[0], args[1]);
     }
